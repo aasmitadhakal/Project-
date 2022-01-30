37a9c8fb8012d516c5db1de95dcc51feaa6cd463
aasmitadhakal <dhakalasmita20@gmail.com>
2022-01-30 12:11:54 +0545

    edited

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,6 @@ const Navbar = ({ toggleSidebar }) => {
     <nav className="bg-white m-2 flex justify-between ">
       <div className="flex ">
         <span onClick={toggleSidebar}>
-<<<<<<< HEAD
-<<<<<<< HEAD
         <svg className="w-6 h-6"
          fill="none" 
          stroke="currentColor"
@@ -15,28 +13,6 @@ const Navbar = ({ toggleSidebar }) => {
               strokeLinejoin="round" 
               strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
               </svg>
-=======
-=======
->>>>>>> 4845700be8565c2486aa5352f6d372012a4f23a9
-          <svg
-            className="stroke-cyan-500 pr-4"
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-<<<<<<< HEAD
->>>>>>> 4845700be8565c2486aa5352f6d372012a4f23a9
-=======
->>>>>>> 4845700be8565c2486aa5352f6d372012a4f23a9
         </span>
         <input
           className=" pr-2 ml-2 mr-2 rounded-xl  border-none placeholder-gray-500 text-black ring-2 ring-gray-300 focus:ring-gray-500 focus-ring-2  "
@@ -46,7 +22,7 @@ const Navbar = ({ toggleSidebar }) => {
           autoComplete="off"
           aria-label="Search talk"
         ></input>
-        <button className="bg-blue-800 px-4 p-1  text-white rounded-full ">
+        <button classame="bg-blue-800 px-4 p-1  text-white rounded-full ">
           Search
         </button>
       </div>
